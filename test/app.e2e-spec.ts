c9c2dbe71e98177bf67f9151a6ef50f8ec2ba6fb
yoo <eorbs255@naver.com>
2025-01-22 22:08:54 +0900

    통합 태스트 코드 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,29 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // DTO에 정의된 속성만 허용
+        forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 들어오면 에러 발생
+        transform: true, // url parameter로 넘어온 값은 자동으로 타입 변환
+      }),
+    );
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('집으로로로로로');
   });
 
-  it('should be 4', () => {
-    expect(2 + 2).toEqual(4);
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+  });
+  describe('/movies/:id', () => {
+    it.todo('GET 200');
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
