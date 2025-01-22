3bd945b9b48a82fc140a35764cb94ab578b18294
yoo <eorbs255@naver.com>
2025-01-22 19:55:48 +0900

    crud 연습

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의된 속성만 허용
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 들어오면 에러 발생
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,29 +1,52 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
+
+  @Get('search')
+  search(@Query('year') seachingYear: string) {
+    return `We are searching for a movie made after aaaaaa : ${seachingYear}`;
+  }
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('/:id')
-  getOne(@Param('id') id: string) {
-    return `This will return one moive ${id}`;
+  getOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.moviesService.getOne(id);
   }
 
   @Post()
-  create() {
-    return `This will create a movie`;
+  create(@Body() movieData: CreateMovieDto) {
+    console.log(movieData);
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: string) {
-    return `This movie will delete a moive ${id}`;
+  remove(@Param('id') id: number) {
+    this.moviesService.deleteOne(id);
+    return `삭제됨 ${id}`;
   }
 
-  @Put('/:id')
-  update(@Param('id') id: string) {
-    return `This movie will update a moive ${id}`;
+  @Patch('/:id')
+  update(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(id, updateData);
   }
 }
