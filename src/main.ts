d59b9ed0fe3cfc6058900c8a1be43845c98ea4ba
yoo <eorbs255@naver.com>
2025-01-22 19:56:05 +0900

    주석 추가

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // DTO에 정의된 속성만 허용
       forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 들어오면 에러 발생
-      transform: true,
+      transform: true, // url parameter로 넘어온 값은 자동으로 타입 변환
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
